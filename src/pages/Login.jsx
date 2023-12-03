1dd6518a18f9a94ded02bf998051a6ebc50ea09a
User192940 <davfedchuk@gmail.com>
2023-12-03 12:02:17 -0600

    adds funcitoning drawer

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
   return (
     <>
     <div className='w-full h-screen'>
-        <img className='hidden sm:block absolute w-full h-full object-cover' src='https://assets.nflxext.com/ffe/siteui/vlv3/dc1cf82d-97c9-409f-b7c8-6ac1718946d6/a6fda18c-4665-445a-9988-12e2871a59ee/US-en-20230911-popsignuptwoweeks-perspective_alpha_website_large.jpg' alt=''/>
+        <img className='block absolute w-full h-full object-cover' src='https://assets.nflxext.com/ffe/siteui/vlv3/dc1cf82d-97c9-409f-b7c8-6ac1718946d6/a6fda18c-4665-445a-9988-12e2871a59ee/US-en-20230911-popsignuptwoweeks-perspective_alpha_website_large.jpg' alt=''/>
         <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
         <div className='fixed w-full px-4 py-24 z-50'>
             <div className='max-w-450px] h-[600px] mx-auto bg-black/75 text-white'> 
@@ -41,7 +41,7 @@ const Login = () => {
                             </p>
                             <p>Need Help?</p>
                         </div>
-                        <p className='py-4 '><span className='text-gray-600'>New to Netflix?</span><Link to='/Signup'> Sign In</Link></p>
+                        <p className='py-4 '><span className='text-gray-600'>New to Netflix?</span><Link to='/Signup'> Sign Up</Link></p>
                     </form>
                 </div>
 
